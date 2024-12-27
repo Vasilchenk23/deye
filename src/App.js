f06e3f02e3204914f7e1cbc0f8575c614b474f4d
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-12-27 09:20:52 +0200

    add slider and header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
+import { Carousel } from "./components/Carousel";
+import { Main } from "./components/Main";
+import carouselData from "./data/carouselData.json"; // JSON імпорт
+
+const slides = carouselData.slides; // Отримуємо масив
 
 function App() {
   return (
     <div className="App">
-     
+      <Main/>
+      <Carousel data={slides} /> {/* Передаємо масив у компонент */}
     </div>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+export const Main = () => {
+    return(
+        <>
+        <header>
+            <div className="logo-item">
+                <img src="./images/logo.png" alt="" />
+            </div>
+            <div className="item-menu">
+                <h1>Головна</h1>
+                <h1>Рiшення</h1>
+                <h1>Продукцiя</h1>
+                <h1>Контакти</h1>
+            </div>
+        </header>
+        </>
+    );
+}
