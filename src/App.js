5a87623103dc4e6efcaec3c994aa48b87c393662
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2024-12-27 11:20:44 +0200

    add Solution component and mobile version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import { Carousel } from "./components/Carousel";
+import { Solution } from "./components/Solution";
 import { Main } from "./components/Main";
-import carouselData from "./data/carouselData.json"; // JSON імпорт
+import carouselData from "./data/carouselData.json"; 
 
-const slides = carouselData.slides; // Отримуємо масив
+const slides = carouselData.slides; 
 
 function App() {
   return (
     <div className="App">
       <Main/>
-      <Carousel data={slides} /> {/* Передаємо масив у компонент */}
+      <Carousel data={slides} /> 
+      <Solution/>
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,34 @@
+import React, { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { IoMdMenu } from "react-icons/io";
+
 export const Main = () => {
-    return(
-        <>
-        <header>
-            <div className="logo-item">
-                <img src="./images/logo.png" alt="" />
-            </div>
-            <div className="item-menu">
-                <h1>Головна</h1>
-                <h1>Рiшення</h1>
-                <h1>Продукцiя</h1>
-                <h1>Контакти</h1>
-            </div>
-        </header>
-        </>
-    );
-}
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      <header className="header">
+        <div className="logo-item">
+          <img src="./images/logo.png" alt="Logo" />
+        </div>
+        <div className={`item-menu ${menuOpen ? "menu-open" : ""}`}>
+          <h1>Головна</h1>
+          <h1>Рiшення</h1>
+          <h1>Продукцiя</h1>
+          <h1>Контакти</h1>
+        </div>
+        <button className="burger-menu" onClick={toggleMenu}>
+          {menuOpen ? (
+            <IoClose size={40}/>
+          ) : (
+            <IoMdMenu size={40}/>
+          )}
+        </button>
+      </header>
+    </>
+  );
+};
